77a94696ebb276963bd0ae9d1ff20b3627052fd3
Collin Gale <Collin.g086@gmail.com>
2020-03-29 01:55:54 -0600

    Holy crap
    So much crap resulting in very little progress it feels like

--- VR Test/Assets/Steam VR/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
+++ VR Test/Assets/Steam VR/SteamVR/InteractionSystem/Core/Scripts/BodyCollider.cs
@@ -14,7 +14,7 @@ namespace Valve.VR.InteractionSystem
 	public class BodyCollider : MonoBehaviour
 	{
 		public Transform head;
-
+		public float yHead;
 		private CapsuleCollider capsuleCollider;
 
 		//-------------------------------------------------
@@ -27,9 +27,12 @@ namespace Valve.VR.InteractionSystem
 		//-------------------------------------------------
 		void FixedUpdate()
 		{
+			yHead = head.transform.localEulerAngles.y;
 			float distanceFromFloor = Vector3.Dot( head.localPosition, Vector3.up );
 			capsuleCollider.height = Mathf.Max( capsuleCollider.radius, distanceFromFloor );
 			transform.localPosition = head.localPosition - 0.5f * distanceFromFloor * Vector3.up;
+			transform.rotation = Quaternion.Euler(new Vector3(0, yHead, 0));
+			//transform.localRotation = new Quaternion(0,head.transform.localRotation.y,0,0);
 		}
 	}
 }
